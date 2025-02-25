2c1ddd63cddec6764177980551722075024ac528
Tubolayefa <tlayefaah@gmail.com>
2025-02-25 13:35:01 +0100

    mobile nav build

--- src/components/NavBar/MobileNav.tsx
+++ src/components/NavBar/MobileNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Logo from "../ui/Logo";
-import { AlignJustify } from "lucide-react";
+import { AlignJustify, ChevronDown } from "lucide-react";
 import { useNavStore } from "@/store/NavStore";
 
 const MobileNav = () => {
@@ -24,9 +24,30 @@ const MobileNav = () => {
       <div
         className={` ${
           isNavOpen ? "translate-x-0" : "-translate-x-[200%]"
-        } fixed h-screen bg-primary top-0 left-0 w-2/3 duration-150`}
+        } pt-10 fixed flex flex-col items-start h-screen bg-white top-0 left-0 w-2/3 duration-150`}
       >
-        hello
+        <Logo css="h-12 px-2" />
+        <div className="mt-10 w-full flex flex-col gap-[1px] text-white font-semibold">
+          <div className="flex items-end justify-between bg-primary py-2 w-full px-2 ">
+            <span>Products</span>
+            <ChevronDown size={22} />
+          </div>
+
+          <div className="flex items-end justify-between bg-primary py-2 w-full px-2 ">
+            <span>Caliberations</span>
+            <ChevronDown size={22} />
+          </div>
+
+          <div className="flex items-end justify-between bg-primary py-2 w-full px-2 ">
+            <span>Services</span>
+            <ChevronDown size={22} />
+          </div>
+
+          <div className="flex items-end justify-between bg-primary py-2 w-full px-2 ">
+            <span>Search</span>
+            <ChevronDown size={22} />
+          </div>
+        </div>
       </div>
     </nav>
   );
