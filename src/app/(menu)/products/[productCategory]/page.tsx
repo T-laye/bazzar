640d5f22c2a75fc9a2cd7189a28358270df74032
Tubolayefa <tlayefaah@gmail.com>
2025-03-05 13:30:06 +0100

    Remove commented-out ProductCard component from products page layout

--- src/app/(menu)/products/[productCategory]/page.tsx
+++ src/app/(menu)/products/[productCategory]/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         <ProductCard />
         <ProductCard />
         <ProductCard />
-        <ProductCard />
+        {/* <ProductCard /> */}
       </div>
     </ProductsLayout>
   );
